060e473c0388bf322b4997156f667a9ae7ebf8a2
Marian Petrov <35401007+maxremus@users.noreply.github.com>
2025-09-11 12:06:34 +0300

    Update ContactController.java

--- src/main/java/accounting/controller/ContactController.java
+++ src/main/java/accounting/controller/ContactController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import accounting.service.TelegramService;
 
 @Controller
 public class ContactController {
