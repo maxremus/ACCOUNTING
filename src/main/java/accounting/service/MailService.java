a21b16208f2a63f81d0c65fafadddd60d797beab
Marian Petrov <35401007+maxremus@users.noreply.github.com>
2025-09-12 11:54:52 +0300

    Update MailService.java

--- src/main/java/accounting/service/MailService.java
+++ src/main/java/accounting/service/MailService.java
@@ -5,7 +5,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class MailService {
     private final JavaMailSender mailSender;
 
